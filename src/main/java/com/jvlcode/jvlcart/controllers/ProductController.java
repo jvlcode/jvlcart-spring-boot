1efa4d60732feec3b5dc6d6b0d2e6f354f75be9d
JVLcode <81806680+jvlcode@users.noreply.github.com>
2024-11-22 19:46:33 +0530

    Product List Pagination
    updated the ddl-auto for error

--- src/main/java/com/jvlcode/jvlcart/controllers/ProductController.java
+++ src/main/java/com/jvlcode/jvlcart/controllers/ProductController.java
@@ -1,12 +1,17 @@
 package com.jvlcode.jvlcart.controllers;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jvlcode.jvlcart.models.Product;
@@ -20,8 +25,15 @@ public class ProductController {
 	private ProductService productService;
 	
 	@GetMapping
-	public List<Product> getAllProducts() {
-		return productService.getAllProducts();
+	public Map<String, Object> getAllProducts(@RequestParam(defaultValue = "0") int page, 
+            @RequestParam(defaultValue = "3") int size) {
+		Pageable pageable = PageRequest.of(page, size);
+        Page<Product> productPage = productService.getAllProducts(pageable);
+        Map<String, Object> response = new HashMap<>();
+        response.put("count", productPage.getTotalElements());
+        response.put("resPerPage", size);
+        response.put("products", productPage.getContent());
+        return response;
 	}
 	
 	@GetMapping("/category")
--- src/main/java/com/jvlcode/jvlcart/services/ProductService.java
+++ src/main/java/com/jvlcode/jvlcart/services/ProductService.java
@@ -1,8 +1,8 @@
 package com.jvlcode.jvlcart.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.jvlcode.jvlcart.models.Product;
@@ -14,7 +14,7 @@ public class ProductService {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	public List<Product> getAllProducts() {
-		return productRepository.findAll();
+	public Page<Product> getAllProducts(Pageable pageable) {
+		return productRepository.findAll(pageable);
 	}
 }
